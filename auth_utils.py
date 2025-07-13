9d70fef975412696899c4d0217b11bfec66bc701
LUC4S4L <104213885+LUC4S4L@users.noreply.github.com>
2025-07-13 17:32:56 -0500

    Update auth_utils.py

--- auth_utils.py
+++ auth_utils.py
@@ -1,4 +1,4 @@
-import requests
+import urllib.request
 import json
 
 def validar_token(token):
@@ -6,16 +6,15 @@ def validar_token(token):
         raise Exception("Token requerido")
 
     url = "https://c0fmkco8rb.execute-api.us-east-1.amazonaws.com/dev/usuario/validar"
+    req = urllib.request.Request(url, headers={"Authorization": token})
 
     try:
-        response = requests.get(url, headers={"Authorization": token})
-
-        if response.status_code != 200:
-            raise Exception("Token inválido o expirado")
-
-        body = response.json()
-        payload = json.loads(body["body"])["payload"]
-        return payload
+        with urllib.request.urlopen(req) as response:
+            body = response.read()
+            data = json.loads(body)
+            payload = json.loads(data["body"])["payload"]
+            return payload
 
     except Exception as e:
         raise Exception(f"Error al validar token: {str(e)}")
+
